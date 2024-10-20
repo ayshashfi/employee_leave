8be7cffc68ad49c4d85a27dfcfd99385c711e1cc
Ayshath Ashfiya A. <ayshathshfiyaa@gmail.com>
2024-10-20 19:28:53 +0530

    added requirements.txt

--- leave_system/leave_system/urls.py
+++ leave_system/leave_system/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     path('',include('leave_app.urls'))
 ]
 
-urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
